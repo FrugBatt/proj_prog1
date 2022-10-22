7f4a01945d2ba69d4d3cd7ab9a5ec80f0d3e7527
Hugo <hugo.**@ens-paris-saclay.fr>
2022-10-22 23:33:55 +0200

    Main program

---
+++ aritha.ml
@@ -0,0 +1,19 @@
+let usage = "Usage : ./aritha expression.exp"
+
+let compile_exp exp_s file_s =
+  let lexems = Analyseur_lexical.analyse_lexicale exp_s in
+  let ast = Analyseur_syntaxique.analyse_syntaxique lexems in
+  Assembly.write_assembly file_s ast
+
+let _ =
+  if Array.length Sys.argv <= 1 then print_endline usage
+  else let exp_file = Sys.argv.(1) in
+    let ic = open_in exp_file in
+    try
+      let line = input_line ic in
+      let file_s = (String.sub exp_file 0 (String.index exp_file '.'))^".s" in
+      print_endline line;
+      compile_exp line file_s;
+      flush stdout;
+      close_in ic
+    with e -> close_in_noerr ic
